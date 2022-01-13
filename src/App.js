79f82b78d8da1c84a4f9fffd1f8fb4549f5aa370
ValeryKulakov <kulakov.resurs@tut.by>
2022-01-13 22:17:46 +0300

    Add simple WebSocket client

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+const ws = new WebSocket('ws://localhost:5000/echo');
+
+ws.addEventListener('open', () => {
+  console.log('OPEN');
+});
+
+ws.addEventListener('message', message => {
+  console.log('MESSAGE: ', message.data);
+});
+
+ws.addEventListener('error', () => {
+  console.log('ERROR');
+});
+
+ws.addEventListener('close', () => {
+  console.log('CLOSE');
+});
+
 function App() {
+  const sendSomeDataToServer = () => {
+    ws.send('CLIENT clicked:)');
+  }
   return (
-    <div className="App">
-      Hello, webSocket! Trial version.
+    <div onClick={sendSomeDataToServer}>
+      Click me to send some data to server!
     </div>
   );
 }
